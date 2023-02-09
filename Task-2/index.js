ada24c5fc5a6d91ce9d31244427da6be6d26a599
Kozlovska Yuliia <kozlovskayayulya2@gmail.com>
2023-02-09 23:41:43 +0100

    created sectors and got selected number of sector,line and seat

--- Task-2/index.js
+++ Task-2/index.js
@@ -59,7 +59,7 @@ const onSeatSelect = (event) => {
 
   const selectedSeatElem = document.querySelector('.board__selected-seat');
 
-  selectedSeatElem.textContent = `S ${sectorNumber} - L ${lineNumber} - ${seatNumber}`;
+  selectedSeatElem.textContent = `S ${sectorNumber} - L ${lineNumber} - S ${seatNumber}`;
 };
 
 arenaElem.addEventListener('click', onSeatSelect);
