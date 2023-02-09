9e53ab91ff10903e7ae3bb5daf047691edae30b7
Kozlovska Yuliia <kozlovskayayulya2@gmail.com>
2023-02-09 23:40:18 +0100

    created sectors and got selected number of sector,line and seat

--- Task-2/index.js
+++ Task-2/index.js
@@ -18,7 +18,7 @@ const getLineSeats = () =>
     )
     .join('');
 
-function getSectorLine() {
+function getSectorLines() {
   const seatsString = getLineSeats();
   return generateNumbersRange(1, 10)
     .map(
@@ -29,22 +29,39 @@ function getSectorLine() {
     )
     .join('');
 }
+const arenaElem = document.querySelector('.arena');
 
 function renderArena() {
-  const arenaElem = document.querySelector('.arena');
-
-  const lineString = getSectorLine();
+  const linesString = getSectorLines();
 
   const sectorsString = generateNumbersRange(1, 3)
     .map(
       (sectorNumber) =>
         `<div 
       class="sector" 
-      data-sector-number='${sectorNumber}'>${lineString}</div>`
+      data-sector-number='${sectorNumber}'>${linesString}</div>`
     )
     .join('');
 
   arenaElem.innerHTML = sectorsString;
 }
+
+const onSeatSelect = (event) => {
+  const isSeat = event.target.classList.contains('sector__seat');
+
+  if (!isSeat) {
+    return;
+  }
+
+  const seatNumber = event.target.dataset.seatNumber;
+  const lineNumber = event.target.closest('.sector__line').dataset.lineNumber;
+  const sectorNumber = event.target.closest('.sector').dataset.sectorNumber;
+
+  const selectedSeatElem = document.querySelector('.board__selected-seat');
+
+  selectedSeatElem.textContent = `S ${sectorNumber} - L ${lineNumber} - ${seatNumber}`;
+};
+
+arenaElem.addEventListener('click', onSeatSelect);
 
 renderArena();
