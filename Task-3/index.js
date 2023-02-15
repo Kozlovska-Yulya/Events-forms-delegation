db76a136dc39b562b7ad423e8cfb23ca7f530edd
Kozlovska Yuliia <kozlovskayayulya2@gmail.com>
2023-02-15 16:42:15 +0100

    created new task

---
+++ Task-3/index.js
@@ -0,0 +1,65 @@
+const tasks = [
+  { id: 'first-task', text: 'Buy milk', done: false },
+  { id: 'second-task', text: 'Pick up Tom from airport', done: false },
+  { id: 'third-task', text: 'Visit party', done: false },
+  { id: 'fourth-task', text: 'Visit doctor', done: true },
+  { id: 'fifth-task', text: 'Buy meat', done: true },
+];
+
+const listElem = document.querySelector('.list');
+
+const renderTasks = (tasksList) => {
+  const tasksElems = tasksList
+    .sort((a, b) => a.done - b.done)
+    .map(({ text, done }) => {
+      const listItemElem = document.createElement('li');
+      listItemElem.classList.add('list__item');
+      const checkbox = document.createElement('input');
+      checkbox.setAttribute('type', 'checkbox');
+      checkbox.checked = done;
+      checkbox.classList.add('list__item-checkbox');
+      if (done) {
+        listItemElem.classList.add('list__item_done');
+      }
+      listItemElem.append(checkbox, text);
+
+      return listItemElem;
+    });
+
+  listElem.append(...tasksElems);
+};
+
+renderTasks(tasks);
+
+//////
+
+const inputElem = document.querySelector('.task-input');
+const createBtnElem = document.querySelector('.create-task-btn');
+
+function haveValue() {
+  const valueInputElem = inputElem.value;
+  if (!valueInputElem) {
+    return;
+  }
+
+  listElem.innerHTML = '';
+  tasks.push({
+    text: valueInputElem,
+    done: false,
+    id: Math.random().toString(),
+  });
+
+  renderTasks(tasks);
+}
+
+function checkboxClick(event) {
+  const isCheckbox = event.target.classList.contains('.list__item-checkbox');
+  if (!isCheckbox) {
+    return;
+  }
+  const taskData = tasks.find(({ id }) => id === event.target.dataset.id);
+  Object.assign(taskData, { done: event.target.checked });
+}
+
+createBtnElem.addEventListener('click', haveValue);
+listElem.addEventListener('click', checkboxClick);
